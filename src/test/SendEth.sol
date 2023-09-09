e9c2e41d2f1543544d9ac24b7c1c6b613254e55a
d1ll0n <root@pc>
2023-09-09 15:09:28 +0100

    add forceTransferETH fn

--- src/test/SendEth.sol
+++ src/test/SendEth.sol
@@ -2,7 +2,13 @@
 pragma solidity >=0.5.0;
 
 contract SendEth {
-  constructor(address target) payable {
-    assembly { selfdestruct(target) }
-  }
+    constructor(address target) payable {
+        assembly {
+            selfdestruct(target)
+        }
+    }
+}
+
+function forceTransferETH(address target, uint256 amount) {
+    new SendEth{ value: amount }(target);
 }
